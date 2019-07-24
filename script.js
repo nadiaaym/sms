cf68dc5cc34aee46f0efab6db6b5271262b7b74e
Gal Ben Haim <gal.benhaim@solaredge.com>
2019-07-24 13:36:03 +0300

    add logic basic arch

---
+++ script.js
@@ -0,0 +1,32 @@
+var state = {
+  products: [],
+  isDialogRender: false
+};
+
+function init() {
+
+}
+
+function openDialog() {
+
+}
+
+function closeDialog() {
+  
+}
+
+function addProduct() {
+
+}
+
+function editProduct(params) {
+  
+}
+
+function deleteProduct(params) {
+  
+}
+
+function updateView() {
+  
+}
